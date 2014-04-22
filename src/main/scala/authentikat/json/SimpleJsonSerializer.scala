b6420495ea103911f0b85a5e1c8aaf5db05ee8fd
jasongoodwin <jay.michael.goodwin@gmail.com>
2014-04-22 14:11:04 -0400

    change collect to map on the simple json serializer. Considering ripping it out and finding a way to serialize Map[String, Any] with recursion by json4s.

--- src/main/scala/authentikat/json/SimpleJsonSerializer.scala
+++ src/main/scala/authentikat/json/SimpleJsonSerializer.scala
@@ -6,19 +6,21 @@ import java.text.SimpleDateFormat
 object SimpleJsonSerializer {
 
   /**
-   * Simple flat json StringBuilder based serializer.
+   * Simple flat json StringBuilder based serializer. Shouldn't be passed
    * @param fields key value pairs
    * @return json string
    */
   def apply(fields: Seq[(String, Any)]) = {
     "{" +
-      fields.collect {
+      fields.map {
         case (key: String, value: Number) =>
           "\"" + key + "\":" + value
         case (key: String, value: Date) =>
           "\"" + key + "\":" + "\"" + formatDateIso8601(value) + "\""
         case (key: String, value: String) =>
           "\"" + key + "\":" + "\"" + value + "\""
+        case (key: String, value: Any) =>
+          "\"" + key + "\":" + "\"" + value.toString + "\""
       }.mkString(",") +
       "}"
   }
