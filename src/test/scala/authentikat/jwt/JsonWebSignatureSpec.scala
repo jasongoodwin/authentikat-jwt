0f7a7123416afe0deef157fec1845f90afcf069a
Jeff Hann <jeff.hann@redspace.com>
2015-10-13 12:13:12 -0300

    continuing my rough start

--- src/test/scala/authentikat/jwt/JsonWebSignatureSpec.scala
+++ src/test/scala/authentikat/jwt/JsonWebSignatureSpec.scala
@@ -23,6 +23,10 @@ class JsonWebSignatureSpec extends FunSpec with Matchers {
     it("Should generate data for HMAC SHA 512 algorithm") {
       JsonWebSignature(HS512, "someData", "secretKey") should equal(Array[Byte](14, -21, 125, 54, -13, 89, -58, -64, 16, -15, 38, -84, -38, -25, 41, -79, 2, 78, -128, 98, 54, -68, -15, -58, 43, -27, -44, 108, -114, -86, -64, 14, -95, 114, 68, -112, -40, 15, 51, 31, 95, -125, -82, -82, -93, -7, -81, 43, 32, 82, -70, 100, -104, 24, 12, 96, -61, 85, 72, 57, 31, -89, -23, 120))
     }
+
+    it("Should generate data for RSA SHA 256 algorithm") {
+      JsonWebSignature(RS256, "someData", "secretKey") should equal(Array[Byte](34, 43, -124, 2, 46, -78, 28, 3, 6, -75, 107, 20, -84, -34, -27, -90, 35, 35, 20, -89, 37, 106, 27, 94, 25, 78, -25, 96, -74, -2, 98, -96))
+    }
   }
 
 }
