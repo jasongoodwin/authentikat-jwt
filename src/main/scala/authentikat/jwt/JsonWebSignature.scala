94e72c33be4f1fd5341f608adeca12cbadbd4c45
Jeff Hann <jeff.hann@redspace.com>
2015-10-13 10:32:30 -0300

    getting started with new rsa sha-256 encryption

--- src/main/scala/authentikat/jwt/JsonWebSignature.scala
+++ src/main/scala/authentikat/jwt/JsonWebSignature.scala
@@ -4,6 +4,12 @@ import javax.crypto.spec.SecretKeySpec
 import javax.crypto.Mac
 import org.apache.commons.codec.binary.Hex
 
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.SecureRandom;
+import java.security.Security;
+import java.security.Signature;
+
 /**
  * Json Web Algorithms for Encrypting JWS.
  * These generate a one way hash (of claims) with a secret key.
@@ -24,18 +30,41 @@ object JsonWebSignature {
       case "HS256" => apply(HS256, data, key)
       case "HS384" => apply(HS384, data, key)
       case "HS512" => apply(HS512, data, key)
-      case "none" => apply(NONE, data, key)
+      case "RS256" => apply(RS256, data, key)
+      case "none" => apply(none, data, key)
       case x => throw new UnsupportedOperationException(x + " is an unknown or unimplemented JWT algo key")
     }
   }
 
   def apply(algorithm: Algorithm, data: String, key: String = null): Array[Byte] = {
     algorithm match {
+      case RS256 => RsaSha("RsaSHA256", data, key)
       case HS256 => HmacSha("HmacSHA256", data, key)
       case HS384 => HmacSha("HmacSHA384", data, key)
       case HS512 => HmacSha("HmacSHA512", data, key)
       case NONE => Array.empty[Byte]
       case x => throw new UnsupportedOperationException(x + " is an unknown or unimplemented JWT algo key")
+    }
+  }
+
+  private case object RsaSha {
+    def apply(algorithm: String, data: String, key: String): Array[Byte] = {
+      val _key = Option(key).getOrElse(throw new IllegalArgumentException("Missing key for JWT encryption via " + algorithm))
+
+      KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA", "BC")
+
+      // this should probably be defined by the algorithm parameter
+      keyGen.initialize(512, new SecureRandom())
+
+      // we should take the provided key
+      KeyPair keyPair = keyGen.generateKeyPair()
+      Signature signature = Signature.getInstance("SHA1withRSA", "BC")
+
+      signature.initSign(keyPair.getPrivate(), new SecureRandom())
+
+      signature.update(data.getBytes)
+
+      signature.sign()
     }
   }
 
@@ -60,8 +89,9 @@ object JsonWebSignature {
 
   case object HS512 extends Algorithm
 
+  case object RS256 extends Algorithm 
+
   //  private sealed abstract class UnimplementedAlgorithm extends Algorithm
-  //  private case object RS256 extends UnimplementedAlgorithm //Recommended implementation
   //  private case object RS384 extends UnimplementedAlgorithm
   //  private case object RS512 extends UnimplementedAlgorithm
   //  private case object ES256 extends UnimplementedAlgorithm //Recommended+ implementation
